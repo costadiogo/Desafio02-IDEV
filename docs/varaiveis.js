92377431668e3b4d4e135abd19c0f443f86713fa
Diogo Costa de Paula <costadiogo30@gmail.com>
2020-08-10 20:51:23 -0300

    Correção do github pages

---
+++ docs/varaiveis.js
@@ -0,0 +1,44 @@
+    //Suas variáveis abaixo
+
+    const linkAvatarMemberA = "https://avatars0.githubusercontent.com/u/62573994?s=460&u=876fa58f6a433c6a3caf2fc253b88b71c2800590&v=4";
+ 
+    let linkGithubMemberA = "https://github.com/costadiogo";
+    
+    let nameMemberA = "Diogo Costa";   
+    
+    let comunnityName = "IDEV";
+    
+    let challengeTitle = "Variáveis";
+    
+    let technologiesYouLearned = ["Git", "GitHub", "JavaScript"];
+    
+    let githubUserNameMemberA = "costadiogo";
+    
+      
+    let professionalObjetive = "Fullstack Developer";
+    
+    let yearYourObjective = 2022;
+    
+    let city = "Navegantes";
+    let day = 10;
+    let month = "agosto";
+    let year = 2020;
+    
+let money = 7000.00;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
