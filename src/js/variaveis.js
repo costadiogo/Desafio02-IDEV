5615c42769b7ca50201eb3eee472f43a6124d43b
Airton <airtonfrance@gmail.com>
2020-08-11 09:45:50 -0400

    Add variaveis do membro B

--- src/js/variaveis.js
+++ src/js/variaveis.js
@@ -1,10 +1,13 @@
     //Suas variáveis abaixo
 
     const linkAvatarMemberA = "https://avatars0.githubusercontent.com/u/62573994?s=460&u=876fa58f6a433c6a3caf2fc253b88b71c2800590&v=4";
- 
+    const linkAvatarMemberB = "https://avatars2.githubusercontent.com/u/67306463?s=400&u=5ada7d6d1b3877ba7a18be033a06bead50c81ce0&v=4";
+     
     let linkGithubMemberA = "https://github.com/costadiogo";
+    let linkGithubMemberB = "https://github.com/airtonfranca";
     
-    let nameMemberA = "Diogo Costa";   
+    let nameMemberA = "Diogo Costa";
+    let nameMemberB = "Airton França"; 
     
     let comunnityName = "IDEV";
     
@@ -13,6 +16,7 @@
     let technologiesYouLearned = ["Git", "GitHub", "JavaScript"];
     
     let githubUserNameMemberA = "costadiogo";
+    let githubUserNameMemberB = "airtonfranca";
     
       
     let professionalObjetive = "Fullstack Developer";
